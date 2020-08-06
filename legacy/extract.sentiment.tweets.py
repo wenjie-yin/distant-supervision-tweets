75c463bea9196ffa3a076ff5c648ebf3ada0061f
Wenjie <w.yin@qmul.ac.uk>
2020-08-06 18:00:41 +0100

    criterion and distantsupervisor classes tested. positive_negative in keywords based on draperunner/distant-supervised-tweets but emojis modified. need to analyse resulting tweets

--- legacy/extract.sentiment.tweets.py
+++ legacy/extract.sentiment.tweets.py
@@ -1,8 +1,10 @@
 import json
 import sys
 import re
+from utils.keywords import positive_negative
 
-tokens = {'pos': [':)', ':-)', ': )', ':D', '=)'], 'neg': [':(', ':-(', ': (']}
+tokens = {'pos': [':)', ':-)', ': )', ':D', '=)'], 'neg': [':(', ':-(', ': (']}  # pos: 77 - neg: 15; filter rt: pos: 9 - neg: 4
+# tokens = positive_negative  # pos: 230 - neg: 3639; filter rt: pos: 112 - neg: 1104
 
 if len(sys.argv) < 2:
     sys.exit()
@@ -18,7 +20,7 @@ with open(fname, 'r') as fh, open(fname.replace('.json', '-pos.json'), 'w') as f
         try:
             tweet = json.loads(line)
 
-            if 'text' in tweet and 'lang' in tweet and tweet['lang'] == 'en':
+            if all(['text' in tweet, 'lang' in tweet, tweet['lang'] == 'en', 'retweeted_status' not in tweet]):
                 alltweets += 1
                 ispos = 0
                 isneg = 0
