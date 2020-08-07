a0d319aff92d09f5a044a72834e986f014a0f05e
Wenjie <acw540@frontend8.apocrita>
2020-08-07 19:41:02 +0100

    add count, can be used to only get a small number of samples

--- get_english.py
+++ get_english.py
@@ -5,14 +5,19 @@ if len(sys.argv) < 2:
     sys.exit()
 
 fname = sys.argv[1]
+count = 0
 
 with open(fname, 'r') as fall, open(fname.replace('.json', '-eng.json'), 'w') as feng:
-    for line in fh:
+    for line in fall:
         line = line.strip()
         try:
             tweet = json.loads(line)
             if 'text' in tweet and 'lang' in tweet and tweet['lang'] == 'en':
                 feng.write(line + '\n')
+            count += 1
+            #if count >= 200:
+                #break
         except:
             continue
-
+        
+	
