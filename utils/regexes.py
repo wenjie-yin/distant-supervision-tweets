ce82b3c680cc321ab2fb8b5fdcb2f1dafae55f22
Wenjie <w.yin@qmul.ac.uk>
2020-11-04 08:51:25 +0000

    word and punct regex

--- utils/regexes.py
+++ utils/regexes.py
@@ -3,3 +3,5 @@ emoticon_regex = r"""(\s|\A)(?:[<>]?[:;=8][\-o\*\']?[\)\]\(\[dDpP/\:\}\{@\|\\]|[
 url_regex = r"""https?:\/\/\S+\b|www\.(\w+\.)+\S*"""
 
 username_regex = r"""(?:@[\w_]+)"""
+
+word_or_punct_regex = r"\w+|[^\w\s]+"
