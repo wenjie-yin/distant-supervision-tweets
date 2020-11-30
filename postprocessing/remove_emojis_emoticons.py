ab16478daaa9e52f126072619aae1fe4010f0697
Wenjie <w.yin@qmul.ac.uk>
2020-11-30 15:52:44 +0000

    replace url and user names with special tokens instead of nothing. extra separation step before removing emoticons. pad non-space non-letter characters with space. add try except for text file instead of json

--- postprocessing/remove_emojis_emoticons.py
+++ postprocessing/remove_emojis_emoticons.py
@@ -14,15 +14,30 @@ output_filename = '{}/{}-cleaned.txt'.format(parent, input_path.name.split('.')[
 
 with open(input_filename, 'r') as fi, open(output_filename, 'wb') as fo:
     for line in fi:
-        line = line.strip()
-        tweet = json.loads(line)
-        text = tweet['text']
+        try:
+            line = line.strip()
+            tweet = json.loads(line)
+            text = tweet['text']
+        except json.decoder.JSONDecodeError:
+            text = line
         text = ' '.join(text.split())
         text = text.encode('ascii', 'ignore').decode()
-        text = re.sub(emoticon_regex, '', text)
-        text = re.sub(url_regex, '', text)
-        text = re.sub(username_regex, '', text)
+
+        # using placeholders first, so that there is no interaction with the next step
+        text = re.sub(url_regex, 'aurlthatis', text)
+        text = re.sub(username_regex, 'auserthatis', text)
+
+        # this way potential emoticons can be separated from text before removing e.g. lol:(
         text = ' '.join(re.findall(word_or_punct_regex, text))
+        text = re.sub(emoticon_regex, '', text)
+
+        # pad punctuations, then remove extra spaces
+        text = re.sub(r'([^\w\s])', r' \1 ', text)
+        text = re.sub('\s{2,}', ' ', text)
+
+        text = text.replace('aurlthatis', '<url>')
+        text = text.replace('auserthatis', '<user>')
+
         text = text.lower()
         text = text.strip()
         to_write = (text + '\n').encode('utf8')
